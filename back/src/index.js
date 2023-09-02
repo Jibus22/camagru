098c1a86d689dbd2957d6d0090cc08b1357789b8
Le Corre <jibeguitare@hotmail.fr>
2023-09-02 15:57:05 +0200

    🚧 wip(router): router in progress

--- back/src/index.js
+++ back/src/index.js
@@ -3,6 +3,7 @@
 import http from "http";
 import * as db from "./db/index.js";
 import { migrate } from "./db/migration.js";
+import { getBody } from "./utils.js";
 
 try {
   await migrate();
@@ -11,27 +12,117 @@ try {
   console.log(err);
 }
 
-const server = http.createServer(async (req, res) => {
-  const urlPath = req.url;
-  if (urlPath === "/overview") {
-    res.end('Welcome to the "overview page" of the nginX project');
-  } else if (urlPath === "/api") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        product_id: "xyz12u4",
-        product_name: "NginX injector",
-      })
-    );
-  } else {
-    try {
-      const { rows } = await db.query("SELECT * FROM table_test");
-      console.log(rows);
-    } catch (err) {
-      console.error(err);
-    }
-    res.end("Successfully started a server");
+// const server = http.createServer(async (req, res) => {
+//   if (req.url === "/session") {
+//     res.writeHead(200, {
+//       "Access-Control-Allow-Origin": "http://localhost:5173",
+//       "Access-Control-Allow-Headers": "Content-Type",
+//       "Access-Control-Allow-Credentials": true,
+//       "Content-Type": "application/json",
+//     });
+//     if (req.method === "OPTION") {
+//       res.end(JSON.stringify({ message: "niquel michel" }));
+//     } else {
+//       let body = await getBody(req);
+
+//       res.end(JSON.stringify(body));
+//     }
+//   } else if (req.url == "/blablabla") {
+//     try {
+//       const { rows } = await db.query("SELECT * FROM table_test");
+//       console.log(rows);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//     res.end("Successfully started a server");
+//   } else {
+//     res
+//       .writeHead(404, { "Content-Type": "application/json" })
+//       .end(JSON.stringify({ message: "route not found" }));
+//   }
+// });
+
+const bla = {
+  GET: {
+    "/": () => console.log("get /"),
+    "/caca": () => console.log("get caca"),
+  },
+  POST: {
+    "/": () => console.log("post /"),
+    "/caca": () => console.log("post caca"),
+  },
+};
+
+class HttpRouter {
+  constructor() {
+    this.routes = {
+      HEAD: {},
+      GET: {},
+      POST: {},
+      PUT: {},
+      DELETE: {},
+      PATCH: {},
+      OPTION: {},
+      TRACE: {},
+      CONNECT: {},
+    };
   }
+
+  head(route, callback) {
+    this.routes.POST[route] = callback;
+  }
+
+  get(route, callback) {
+    this.routes.GET[route] = callback;
+  }
+
+  post(route, callback) {
+    this.routes.POST[route] = callback;
+  }
+
+  put(route, callback) {
+    this.routes.POST[route] = callback;
+  }
+
+  delete(route, callback) {
+    this.routes.POST[route] = callback;
+  }
+
+  patch(route, callback) {
+    this.routes.POST[route] = callback;
+  }
+
+  option(route, callback) {
+    this.routes.POST[route] = callback;
+  }
+
+  processIncomingHttpMessage(req, res) {
+    const fn = this.routes[req.method][req.url];
+    if (fn == undefined) return;
+    fn(req, res);
+  }
+}
+
+const router = new HttpRouter();
+
+router.get("/", (req, res) => {
+  console.log("yo");
+  res
+    .writeHead(200, { "Content-Type": "application/json" })
+    .end(JSON.stringify({ message: "caca", url: req.url, method: req.method }));
+});
+
+router.post("/", (req, res) => {
+  console.log("yo");
+  res
+    .writeHead(200, { "Content-Type": "application/json" })
+    .end(JSON.stringify({ message: "caca", url: req.url, method: req.method }));
+});
+
+console.log(router.routes);
+
+const server = http.createServer(async (req, res) => {
+  router.processIncomingHttpMessage(req, res);
 });
 
 server.listen(4000, () => {
