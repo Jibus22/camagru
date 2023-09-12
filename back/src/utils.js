a614a42bb0199bf0cd657f6b3b1c3c04ffec35a4
Le Corre <jibeguitare@hotmail.fr>
2023-09-12 18:03:47 +0200

    ✨ feat(http): made a async utility to make post request from backend, with http module. It will be used to make request to outlook api

--- back/src/utils.js
+++ back/src/utils.js
@@ -1,3 +1,6 @@
+import http from "http";
+
+// Promisify the mean to receive the request body from http.IncomingMessage
 export const getBody = (req) => {
   return new Promise((resolve, reject) => {
     try {
@@ -14,4 +17,40 @@ export const getBody = (req) => {
       reject(error);
     }
   });
+};
+
+// Promisify the mean to receive a response from
+// http.ClientRequest (which extends http.OutgoingMessage)
+export const getResponse = (req) => {
+  return new Promise((resolve, reject) => {
+    try {
+      req.on("response", async (res) => {
+        const body = await getBody(res);
+        resolve(body);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+export const httpPost = async (opts, data = {}) => {
+  data = JSON.stringify(data);
+  opts = {
+    port: 4000,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },
+    ...opts,
+  };
+
+  const req = http.request(opts);
+
+  req.write(data);
+  req.end();
+
+  const response = await getResponse(req);
+  return response;
 };
