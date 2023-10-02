23058ea8529dae5622a7ce05ee9b726301190c31
Le Corre <jibeguitare@hotmail.fr>
2023-10-02 18:53:36 +0200

    🐛 fix(infra): making my nginx config file being used by nginx

--- front/src/app/views/edit/superposable.js
+++ front/src/app/views/edit/superposable.js
@@ -1,4 +1,3 @@
-import { data_superposable } from "../data/edit_superposable";
 import { createElement, imageFit } from "../../utils/utils.js";
 
 export const superposable = (edit__main) => {
@@ -74,3 +73,78 @@ export const superposable = (edit__main) => {
 
   return superposable;
 };
+
+const data_superposable = [
+  {
+    img: "/superposable/memeno.png",
+  },
+  {
+    img: "/superposable/memeyes.png",
+  },
+  {
+    img: "/superposable/troll.png",
+  },
+  {
+    img: "/superposable/troll2.png",
+  },
+  {
+    img: "/superposable/troll3.png",
+  },
+  {
+    img: "/superposable/smart.png",
+  },
+  {
+    img: "/superposable/toilet.png",
+  },
+  {
+    img: "/superposable/sadcat.png",
+  },
+  {
+    img: "/superposable/dodge.png",
+  },
+  {
+    img: "/superposable/dodgecred.png",
+  },
+  {
+    img: "/superposable/urfault.png",
+  },
+  {
+    img: "/superposable/motivated.png",
+  },
+  {
+    img: "/superposable/oh.png",
+  },
+  {
+    img: "/superposable/sadmj.png",
+  },
+  {
+    img: "/superposable/burger.png",
+  },
+  {
+    img: "/superposable/cave.png",
+  },
+  {
+    img: "/superposable/christmas.png",
+  },
+  {
+    img: "/superposable/heart.png",
+  },
+  {
+    img: "/superposable/moving.png",
+  },
+  {
+    img: "/superposable/plumber.png",
+  },
+  {
+    img: "/superposable/redtshirt.png",
+  },
+  {
+    img: "/superposable/treepng.png",
+  },
+  {
+    img: "/superposable/tshirt.png",
+  },
+  {
+    img: "/superposable/wine.png",
+  },
+];
